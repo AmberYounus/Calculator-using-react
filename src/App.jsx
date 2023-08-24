14a727d00e9493725ea689cd8ebafb02f6d2983d
Amber Younus <amberyounus22@gmail.com>
2023-08-24 23:06:09 +0500

    calculator

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Appview from './Views/Appview;'
+function App() {
+  return (
+    <>
+  <React.Fragment>
+    <Appview/>
+  </React.Fragment>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Wrapper.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Wrapper =({children})=>{
+return <div className="wrapper">{children}</div>
+}
+export default Wrapper
